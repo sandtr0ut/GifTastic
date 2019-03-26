653bc4c8b317805a5e83863d12d01f66c1c98add
sandtr0ut <crosbyverse@gmail.com>
2019-03-25 17:39:05 -0700

    add ajax call and response logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,51 @@ $(document).ready(function () {
     "George Washington", "Benjamin Franklin", "Abraham Lincoln", "Helen Keller", "John F Kennedy", "Martin Luther King, Jr", "Franklin D. Roosevelt", "Albert Einstein", "John F. Kennedy", "Robert F. Kennedy", "Thomas Jefferson", "Thomas Alva Edison", "Frederick Douglass", "Susan B. Anthony", "Theodore Roosevelt", "Henry Ford", "Bill Gates", "Steve Jobs", "Jesse Owens", "Jackie Robinson", "Howard Hughes", "Amelia Earhart", "Andrew Carnegie", "Barack Obama", "Mark Twain", "Joseph Smith, Jr.", "J.P. Morgan", "Neil Armstrong", "Malcolm X", "John Glenn", "Bob Dylan", "Billie Jean King", "Chuck Yeager", "Elvis Presley", "Ernest Hemingway", "Marilyn Monroe", "Muhammad Ali", "Jimi Hendrix", "Hugh Hefner"
   ];
   
-  
+  // Function to Display Images on name-button Clicks
+  function displayImages() {
+    
+    var VIP = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZQlhsr3WWOlLaitiQkikLDYzzfYU6tci&q=" + VIP + "&limit=10&offset=0&rating=G&lang=en";
+    
+    // ajax call for the click event
+    $.ajax({
+      type: "method",
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      
+      // create an array with the 10 responses
+      var results = response.data;
+      
+      // loop through each result
+      for (let j = 0; j < results.length; j++) {
+            
+    
+      // create new div element for images
+      var imageDiv = $("<div class='images'>");
+      
+      // store rating info
+      var rating = results[j].rating;
+      
+      // create p-element to display rating
+      var ratingDisplay = $("<p>").text("Rating: " + rating);
+      
+      // create an image tag
+      var VIPimg = $("<img>");
+      
+      // give the image a src attribute
+      VIPimg.attr("src", results[j].images.fixed_width.url);
+      
+      // append the displayed image and rating to the image div
+      imageDiv.append(VIPimg);
+      imageDiv.append(ratingDisplay);
+      
+      // put the whole shootin' match in the image area
+      //   ...on top of any previous images
+      $("#image-area").prepend(imageDiv);
+    }
+  });
+}
   // Function to Display Buttons
   //  >> runs:
   //    - on load
@@ -58,7 +102,7 @@ $(document).ready(function () {
   })
   
   // Click Event for Name Buttons
-  
+  $(document).on("click", ".name-button", displayImages);
   
   
 });
